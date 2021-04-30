8ea403446c436674f9f2e233812361cd69ff1ac9
Jean Ximenes <jean.mesquita@mprj.mp.br>
2021-04-30 20:11:30 -0300

    feat: primeira versão da dog api

---
+++ src/index.js
@@ -0,0 +1,112 @@
+import './index.css';
+
+const React = require('react');
+const ReactDom = require('react-dom');
+const axios = require('axios');
+
+class DogsBreed extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      imgURL: "",
+      breed: [""],
+      select: ""
+    }
+  }
+
+  getDogPicture = () => {
+    const { select } = this.state;
+    let url = "https://dog.ceo/api/breed/" + select + "/images/random";
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({
+          imgURL: response.data.message
+        });
+        console.log(response.data.message)
+      })
+      .catch(err => {
+        console.log("error fetching image");
+      });
+  };
+
+  getBreed = () => {
+    const {breed} = this.state;
+    axios
+      .get("https://dog.ceo/api/breeds/list")
+      .then(response => {
+        this.setState({
+          breed: breed.concat(response.data.message)
+        })
+      })
+      .catch(err => {
+        console.log("error fetching list");
+      });
+  }
+
+  getRandomPicture = () => {
+    const { breed } = this.state;
+    const randomDog = Math.floor(Math.random()*breed.length);
+    const choice = (randomDog === 0 ? "wolfhound" : breed[randomDog]);
+    console.log(randomDog);
+    let url = "https://dog.ceo/api/breed/" + choice + "/images/random"
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({
+          imgURL: response.data.message,
+          select: choice
+        });
+      })
+      .catch(err => {
+        console.log("error fetching image");
+      });
+      console.log(url);
+  };
+
+  handleSelect = (e) => {
+    this.setState({
+      select: e.target.value
+    })
+  }
+
+  componentDidMount() {
+    this.getBreed();
+  }
+
+  render() {
+    
+    const { breed, imgURL, select} = this.state;
+    
+    return(
+      <div className="main">
+        <h1>Dogs by Breed</h1>
+        <div class="content">
+          <div>
+            <h3>Option 1: Choose a breed from the select and click submit</h3>
+            <select value={select} onChange={this.handleSelect}>
+              {breed.map(e => 
+                <option value={e}> {e} </option>
+              )}
+            </select>
+            <button id="submit" disabled={!select} onClick={this.getDogPicture}>SUBMIT</button>
+          </div>
+          <div class="lbl-opt">OR</div>
+          <div>
+            <h3>Option 2: Click the random button for a random dog.</h3>
+            <p>Breed: {select}</p>
+            <button onClick={this.getRandomPicture}>RANDOM</button>
+          </div>
+          <div id="box-img">
+            <img alt="dog" src={imgURL} className="height-Pictureure" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDom.render(
+  <DogsBreed />,
+  document.getElementById('root')
+);
